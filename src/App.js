4b8d6887dee22174464995214e62a21d9bd71104
tikkuinen <titunen@gmail.com>
2022-10-09 22:09:42 +0300

    testing api

--- src/App.js
+++ src/App.js
@@ -1,10 +1,28 @@
-import logo from './logo.svg';
+import { useEffect, useState } from 'react';
 import './App.css';
+import axios from 'axios';
 
 function App() {
+  const [juna, setJuna] = useState([]);
+
+  const URL = "https://rata.digitraffic.fi/api/v1/live-trains/station/TPE/JÄS";
+
+  useEffect(() => {
+    // tänne että hakee päivän, ja sitten yhdistää linkkiin? että tämän päivän juna
+    
+    axios.get(URL)
+      .then((response) => {
+        //console.log(response)
+        const train = response.data[0];
+        setJuna(train.trainNumber);
+      })
+
+  }, [])
+  
   return (
     <div>
-      
+      <h1>Trains?</h1>
+      <p>{juna}</p>
     </div>
   );
 }
